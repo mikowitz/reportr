924aa3772ebef378d0b5b49d45c40b0fd41f0ce7
Michael Berkowitz <michael.berkowitz@gmail.com>
2013-04-08 09:35:56 -0400

    clean up implementation

--- lib/reportr.rb
+++ lib/reportr.rb
@@ -5,11 +5,11 @@ require 'active_support/core_ext/string/conversions'
 module Reportr
   def self.report(from, to, opts={})
     seconds = to - from
+    seconds -= (seconds % 60) if opts[:ignore_seconds]
     minutes, seconds = seconds.divmod(60)
     hours, minutes = minutes.divmod(60)
     days, hours = hours.divmod(24)
     result = "%02d:%02d:%02d" % [days, hours, minutes]
-    result += (":%02d" % seconds) if (seconds > 0) && !opts[:ignore_seconds]
-    result
+    seconds == 0 ? result : result + (":%02d" % seconds)
   end
 end
